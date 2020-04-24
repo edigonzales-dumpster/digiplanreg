094b5c171b5050d4b48a053787085733f6d0a039
Stefan Ziegler <edi.gonzales@gmail.com>
2020-04-24 10:29:37 +0200

    initial commit

---
+++ src/main/java/ch/so/agi/digiplanreg/DigiplanregApplication.java
@@ -0,0 +1,13 @@
+package ch.so.agi.digiplanreg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiplanregApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigiplanregApplication.class, args);
+	}
+
+}
